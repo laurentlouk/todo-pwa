16429be1bc579c8601c9c64ea4842c5dfd56090c
Laurent Loukopoulos <laurent.loukopoulos@gmail.com>
2019-02-06 14:21:18 +0100

    add cache for libraries cdn

--- src/sw.js
+++ src/sw.js
@@ -1,17 +1,25 @@
 workbox.skipWaiting()
 workbox.clientsClaim()
 
-self.addEventListener('install', event => {
-  const asyncInstall = new Promise(resolve => {
-    console.log('Waiting to resolve...')
-    setTimeout(resolve, 5000)
+// self.addEventListener('install', event => {
+//   const asyncInstall = new Promise(resolve => {
+//     console.log('Waiting to resolve...')
+//     setTimeout(resolve, 5000)
+//   })
+
+//   event.waitUntil(asyncInstall)
+// })
+
+// self.addEventListener('activate', event => {
+//   console.log('activate')
+// })
+
+// Cache lybraries under cdn-cach name
+workbox.routing.registerRoute(
+  new RegExp('https:.*min\.(css|js)'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'cdn-cach'
   })
-
-  event.waitUntil(asyncInstall)
-})
-
-self.addEventListener('activate', event => {
-  console.log('activate')
-})
+)
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
